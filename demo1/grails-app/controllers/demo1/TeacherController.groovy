d59e536eb85d150c548801784ddd5867fb2161aa
KabindraKattel <kattelk4b1n@gmail.com>
2021-09-10 16:02:26 +0545

    hey

--- demo1/grails-app/controllers/demo1/TeacherController.groovy
+++ demo1/grails-app/controllers/demo1/TeacherController.groovy
@@ -1,17 +1,18 @@
 package demo1
 
-import grails.plugin.springsecurity.SpringSecurityService
 import grails.plugin.springsecurity.annotation.Secured
 
 @Secured('ROLE_TEACHER')
 class TeacherController {
+    def teacherService = new TeacherService()
     def springSecurityService
 
 
     def index(){
         def username = ((User) springSecurityService.currentUser).username
-
-        [username:username]
+        teacherService.setUsername(username)
+        teacherService.allSubjectsName
+//        [username:username]
     }
 
 }
--- demo1/grails-app/services/demo1/TeacherService.groovy
+++ demo1/grails-app/services/demo1/TeacherService.groovy
@@ -8,42 +8,44 @@ import java.util.stream.Collectors
 class TeacherService {
     String username
 
-    Student getLoggedInTeacher(){
-        return username==null?null:Student.findByUser_name(username)
-    }
-
-    private Stream getStream() {
-        Stream.findById(getLoggedInStudent().streamId)
+    Teacher getLoggedInTeacher(){
+        username==null?null:Teacher.findByUser_name(username)
     }
 
     private List<Subject> getAllSubjects() {
-        def stream = getStream()
-        stream == null?[]: Subject.findAllByStream(stream)
-    }
-
-    private List<Teacher> getAllTeachers(){
-        def teacherCriteria = Teacher.createCriteria()
-        def subjectIds = allSubjects == []?[]:allSubjects.stream().map({ subject -> subject.id }).collect(Collectors.toList())
-        subjectIds.each {id-> println(id)}
-        def teachers = teacherCriteria.list {
-            subjects {
-                subjectIds.each {id -> eq("id",id)}
+        Long teacherId = loggedInTeacher.id
+        def subjects = Subject.createCriteria().list {
+            teachers {
+                eq("id", teacherId)
             }
-        } as List<Teacher>
-        subjectIds==[]?[]:teachers
+        } as List<Subject>
+        subjects == []?[]:subjects
+//        def stream = getStream()
+//        stream == null?[]: Subject.findAllByStream(stream)
     }
 
-    String getStreamName(){
-        def stream = getStream()
-        stream == null?null:stream.name
+    private List<Stream> getAllStreams() {
+        def subjectIds = allSubjects.stream().map({ subject -> subject.id }).collect(Collectors.toList())
+        Stream.createCriteria().list {
+            subjectIds.each {
+                id -> eq("id", id)
+            }
+        } as List<Stream>
+    }
+
+    private List<Student> getAllStudents(){
+        def streamIds = allStreams.stream().map({ stream -> stream.id }).collect(Collectors.toList())
+        def subjects = Student.createCriteria().list {
+                streamIds.each {id -> eq("id",id)}
+        } as List<Subject>
+    }
+
+    String getAllStreamNames(){
+        allStreams.stream().map({ stream -> stream.name }).collect(Collectors.toList())
     }
 
     List<String> getAllSubjectsName(){
         allSubjects.stream().map({ subject -> subject.name }).collect(Collectors.toList())
-    }
-
-    List<String> getAllTeachersName(){
-        allTeachers.stream().map({ teacher -> teacher.name }).collect(Collectors.toList())
     }
 
 }
